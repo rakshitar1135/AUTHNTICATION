be2a55d1a6b5d37bb35c2208f285496685adae66
rakshitar1135 <rakshitaraju2000@gmail.com>
2022-12-11 18:37:56 +0530

    Authstate with context

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -19,40 +19,48 @@ const AuthForm = () => {
     const enteredEmail = emailInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
 
-    // optional: Add validation
+    SetIsLoading(true)
+    let url;
 
     if (isLogin) {
+      url='https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD9CllfV0tx_4qSf2nSzKyn4Q0BlqazDMM '
     } else {
-      fetch(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD9CllfV0tx_4qSf2nSzKyn4Q0BlqazDMM ",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      ).then((res) => {
-        SetIsLoading(false)
+      url='https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD9CllfV0tx_4qSf2nSzKyn4Q0BlqazDMM '
+    }
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => {
+        SetIsLoading(false);
         if (res.ok) {
-          // ...
+          return res.json();
         } else {
           return res.json().then((data) => {
-            // show an error modal
             let errorMessage = 'Authentication failed!';
-           // if (data && data.error && data.error.message) {
-              //errorMessage = data.error.message;
-           // }
-            alert(errorMessage);
+            // if (data && data.error && data.error.message) {
+            //   errorMessage = data.error.message;
+            // }
+
+            throw new Error(errorMessage);
           });
         }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        alert(err.message);
       });
-    }
   };
+
 
   return (
     <section className={classes.auth}>
@@ -86,5 +94,4 @@ const AuthForm = () => {
     </section>
   );
 };
-
 export default AuthForm;
